7055f253b7f1a60121d999845e6086232d5e9c49
Binu Sharath <82375489+BinuSindarth@users.noreply.github.com>
2021-12-13 10:09:14 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,6 @@ import "firebase/auth";
 
 export const auth = firebase
   .initializeApp({
-    apiKey: "AIzaSyAWxnWdUdB7q5Oa3YuyZEAL0Kr2HVvwgH8",
-    authDomain: "alanbinu-mychat.firebaseapp.com",
-    projectId: "alanbinu-mychat",
-    storageBucket: "alanbinu-mychat.appspot.com",
-    messagingSenderId: "652955793895",
-    appId: "1:652955793895:web:5b7e8e8d04c5dd2646a542",
-    measurementId: "G-CZ88D7MT78"
+    //insert your app config
   })
   .auth();
